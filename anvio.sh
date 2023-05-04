4d058f101089fcc88b08074a0b4fa0fc1c686c2e
Thibault <90853477+THibaultBret@users.noreply.github.com>
2023-05-04 14:01:23 +0200

    Add files via upload

---
+++ anvio.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+#SBATCH -c 8 --mem 4G --output=Acetic.xmfa --time 12:00:00 --mail-user=thibault.bret@sund.ku.dk --mail-type=ALL
+module load anvio/7.1
+
+for i in $(ls *.fna); do
+    mv $i "${i%.*}".fa
+done
+
+for i in $(ls *.fa); do
+    anvi-script-reformat-fasta $i -l 0 --simplify-names --seq-type NT -o "${i}.fixed"
+    mv ${i}.fixed $i
+    if [ -f $i.db ]; then
+        echo "$i.db already exists."
+    else
+         anvi-gen-contigs-database -f $i -o ${i%.*}_CONTIGS.db -T 4
+    fi
+done
+
+# Make an external genomes database
+for i in $(ls *fa); do
+    f=${i%%.*}
+    echo -e ${f}"\t"${f}_CONTIGS.db
+done | grep -v 'FIXED' > external_genomes.txt
+
